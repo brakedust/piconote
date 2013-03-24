0bd1a67da9bb4669dcdd39b10465b0249ed48ba1
Brad Campbell <bradlcampbell@gmail.com>
2013-03-23 21:35:19 -0400

    More renaming for change name to piconote

--- piconote/notes.py
+++ piconote/notes.py
@@ -9,7 +9,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 from datetime import datetime
 
-from mininote.dbtypes import GUID
+from piconote.dbtypes import GUID
 
 # create a configured "Session" class
 
