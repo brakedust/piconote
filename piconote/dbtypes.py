81c9c2b19f6b2ab2c4d811b35d655033c9bec9a5
brakedust <bradlcampbell@gmail.com>
2013-04-12 12:45:36 -0400

    added GUID_binary type

--- piconote/dbtypes.py
+++ piconote/dbtypes.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from sqlalchemy import CHAR, VARCHAR
+from sqlalchemy import CHAR, VARCHAR, BINARY
 
 from sqlalchemy.types import TypeDecorator
 from sqlalchemy.dialects.postgresql import UUID as pgUUID
@@ -7,7 +7,7 @@ from sqlalchemy.dialects.postgresql import UUID as pgUUID
 import uuid
 import json
 
-class GUID(TypeDecorator):
+class GUID_Char(TypeDecorator):
     """Platform-independent GUID type.
 
     Uses Postgresql's UUID type, otherwise uses
@@ -41,7 +41,45 @@ class GUID(TypeDecorator):
             return uuid.UUID(value)
             
     def new_uuid():
-        return uuid.uuid4()
+        return uuid.uuid1()
+        
+class GUID_Binary(TypeDecorator):
+    """Platform-independent GUID type.
+
+    Uses Postgresql's UUID type, otherwise uses
+    CHAR(32), storing as stringified hex values.
+
+    """
+    impl = CHAR
+
+    def load_dialect_impl(self, dialect):
+        if dialect.name == 'postgresql':
+            return dialect.type_descriptor(pgUUID)
+        else: 
+            return dialect.type_descriptor(BINARY)
+
+    def process_bind_param(self, value, dialect):
+        if value is None:
+            return value
+        elif dialect.name == 'postgresql':
+            return str(value)
+        else:
+            if not isinstance(value, uuid.UUID):
+                return "{0}".format(str(uuid.UUID(value).bytes)[2:-1])  
+            else:
+                # binary string
+                return "{0}".format(str(value.bytes)[2:-1])  
+
+    def process_result_value(self, value, dialect):
+        if value is None:
+            return value
+        else:
+            return uuid.UUID(bytes = eval("b'{0}'".format(value)))
+            
+    def new_uuid():
+        return uuid.uuid1()
+        
+
 
 class JSONDict(TypeDecorator):
     """Represents an immutable structure as a json-encoded string.
